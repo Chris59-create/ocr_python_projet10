4247c3698ce5f7a86a241d419a4fc9d7b71bfce4
Chris Create <chris59create@gmail.com>
2023-04-17 15:11:17 +0200

    Add hashin of password

--- app_authentication/serializers.py
+++ app_authentication/serializers.py
@@ -1,19 +1,55 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from rest_framework.serializers import api_settings
 
-User = get_user_model()
+USER_MODEL = get_user_model()
 
 
 class SignUpSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = User
+        model = USER_MODEL
         fields = ('email', 'first_name', 'last_name', 'password')
+        extra_kwargs = {
+            'email': {
+                'error_messages': {
+                    'unique': 'Un utilisateur avec cet email existe déjà'
+                }
+            },
+        }
 
-    def validate_email(self, value):
+    def validate_password(self, value):
+        # Hashes the password
+        return make_password(value)
 
-        if User.objects.filter(email=value).exists():
 
-            raise serializers.ValidationError("L'email est déjà utilisé")
+class LoginSerializer(serializers.ModelSerializer):
 
-        return value
+    class Meta:
+        model = USER_MODEL
+        fields = ['email', 'password']
+
+    def validate(self, attrs):
+
+        email = attrs.get('email', None)
+        password = attrs.get('password', None)
+
+        if not email:
+            raise serializers.ValidationError(
+                'Une adresse email est requise pour se connecter'
+            )
+
+        if not password:
+            raise serializers.ValidationError(
+                'Le mot de passe est requis pour se connecter'
+            )
+
+        user = authenticate(email=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError(
+                "Pas d'utilisateur correspondant à cet email et mot de passe"
+            )
+
+        return attrs
